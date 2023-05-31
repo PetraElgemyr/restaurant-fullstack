e04260807039e2d42601058474974714d230d469
Hamajaff <jaff226@gmail.comm>
2023-05-31 21:57:12 +0200

    (feat): Created confirmation page

--- fed22s-therestaurant/src/Router.tsx
+++ fed22s-therestaurant/src/Router.tsx
@@ -3,6 +3,7 @@ import { Layout } from "./components/Layout";
 import { Home } from "./components/Home";
 import { Booking } from "./components/Booking";
 import Contact from "./components/Contact";
+import Confirmation from "./components/Confirmation";
 
 export const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ export const router = createBrowserRouter([
       {
         path: "/contact",
         element: <Contact></Contact>,
+      },
+      {
+        path: "/confirmation",
+        element: <Confirmation></Confirmation>,
       },
     ],
   },
