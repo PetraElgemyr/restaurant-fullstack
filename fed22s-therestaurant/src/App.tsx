e0981c776adb23e115fd6ead0d58f5df72d80b3d
petraelgemyr <petra.elgemyr@gmail.com>
2023-06-01 00:55:43 +0200

    (feat): add contexts in app.tsx, around router

--- fed22s-therestaurant/src/App.tsx
+++ fed22s-therestaurant/src/App.tsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
-
+import { useEffect, useReducer } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router";
+import { BookingContext } from "./contexts/BookingContext";
+import { BookingsReducer } from "./reducers/BookingsReducer";
+import { BookingDispatchContext } from "./contexts/BookingDispatchContext";
 
 function App() {
+  const [bookings, dispatch] = useReducer(BookingsReducer, []);
+
+  useEffect(() => {
+    //hämta alla bokningar
+  }, []);
+
   return (
     <>
-      <RouterProvider router={router}></RouterProvider>
+      <BookingContext.Provider value={bookings}>
+        <BookingDispatchContext.Provider value={dispatch}>
+          <RouterProvider router={router}></RouterProvider>
+        </BookingDispatchContext.Provider>
+      </BookingContext.Provider>
     </>
   );
 }
