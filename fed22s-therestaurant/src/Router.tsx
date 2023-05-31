d0837155d478f3da282fa84e7cbd8765306e4342
petraelgemyr <petra.elgemyr@gmail.com>
2023-05-31 13:18:46 +0200

    (feat): add basic structure

---
+++ fed22s-therestaurant/src/Router.tsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { Home } from "./components/Home";
+import { Booking } from "./components/Booking";
+import { Contact } from "./components/Contact";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/booking",
+        element: <Booking></Booking>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+    ],
+  },
+]);
---
+++ fed22s-therestaurant/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <>
+      <ul>
+        <Link to="/">(LOGO KOMMER)</Link>
+      </ul>
+    </>
+  );
+};
