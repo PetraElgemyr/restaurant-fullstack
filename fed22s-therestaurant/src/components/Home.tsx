a648549446d58e7f13fd270aa30cc7e4925188cc
petraelgemyr <petra.elgemyr@gmail.com>
2023-06-01 00:53:05 +0200

    (feat): add tests and unused test code

--- fed22s-therestaurant/src/components/Home.tsx
+++ fed22s-therestaurant/src/components/Home.tsx
@@ -3,8 +3,15 @@ import { Link } from "react-router-dom";
 export const Home = () => {
   return (
     <>
-      <p>Home funkar!</p>
-      <Link to="/contact"></Link>
+      <h2>Välkommen till Bakgården!</h2>
+      <p>
+        Vi är en mysig liten restaurang som serverar högkvalitativa rätter på
+        närodlade ekologiska varor.
+      </p>
+      <Link to="/booking">
+        <button>Boka bord</button>
+      </Link>
+      <Link to="/contact">Kontakta oss</Link>
     </>
   );
 };
---
+++ fed22s-therestaurant/src/reducers/CurrentBookingReducer.ts
@@ -0,0 +1,39 @@
+/**
+ *
+ * EJ AKTUELL, BARA PETRAS KLADD :)
+ *
+ */
+
+// import { Booking } from "../models/Booking";
+
+// export interface IAction {
+//   type: string;
+//   payload: string /*Glöm ej att stringifya alla payloads som vi skickar med dispatch-anropet. Alla ska va samma datatyp.
+//       Glöm ej att parsa payload från string till json när vi tar emot och ska använda payload i vår reducer */;
+// }
+
+// export const BookingsReducer = (booking: Booking, action: IAction): Booking => {
+//   switch (action.payload) {
+//     case "added": {
+//       //Ta emot antalet gäster
+//       //Få datum från kalender
+//       //Se vilka bokningsobjekt vars "date" är det valda datumet
+//       //Kolla på sittning 1 och 2 det datumet om det är 15 bokade bord eller om det finns lediga.
+//       //(gör loop och if(sitting===1) addera booking.bookedTables och samma för sitting=2 för den dagen)???
+//       //Visa tillgänglig sittning ledig det datumet
+//       //Ta emot vilken sittning, 1 eller 2
+//       //Ta emot namn, mejl, mobilnr från payload till nytt User-objekt.
+//       //Skapa ett nytt bokningsobjeket
+//       //Lägg till objektet i listan med bokningar
+//     }
+
+//     case "deleted": {
+//       break;
+//     }
+
+//     default:
+//       break;
+//   }
+
+//   return booking;
+// };
