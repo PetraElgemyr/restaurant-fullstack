1e2097251f75a1ba626931f0ecbf820886c528a0
petraelgemyr <petra.elgemyr@gmail.com>
2023-06-01 23:08:13 +0200

    (feat): add new properties to context interface

--- fed22s-therestaurant/src/contexts/BookingsContext.ts
+++ fed22s-therestaurant/src/contexts/BookingsContext.ts
@@ -4,9 +4,13 @@ import { Booking, defaultBooking } from "../models/Booking";
 export interface IBookingsContext {
   bookings: Booking[];
   currentBooking: Booking;
+  firstSittingTablesLeft: number;
+  secondSittingTablesLeft: number;
 }
 
 export const BookingsContext = createContext<IBookingsContext>({
   bookings: [],
   currentBooking: defaultBooking,
+  firstSittingTablesLeft: 0,
+  secondSittingTablesLeft: 0,
 });
