f916d0cd5f8449a885b67166dcfc02d869e1b189
Jennifer McAllister <113523615+jennifer-mcallister@users.noreply.github.com>
2023-06-01 21:26:48 +0200

    (fix): spellcheck

--- fed22s-backend/models/Booking.js
+++ fed22s-backend/models/Booking.js
@@ -5,7 +5,7 @@ const BookingSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "Booking",
     },
-    seating: {
+    sitting: {
         type: Number,
         required: true,
     },
