ca6e9437b14dc3cefea29b897e434ea5c243b3be
petraelgemyr <petra.elgemyr@gmail.com>
2023-06-07 11:54:33 +0200

    (feat): add dispatch call, change numOfGuests

--- fed22s-therestaurant/src/components/BookGuests.tsx
+++ fed22s-therestaurant/src/components/BookGuests.tsx
@@ -6,22 +6,31 @@ export interface IChooseGuests {
   goToCalendar: () => void;
 }
 
-const numberOfGuests = [1, 2, 3, 4, 5, 6, 7, 8 ,9 ,10, 11, 12];
-
 export const BookGuests = ({ goToCalendar }: IChooseGuests) => {
+  const numberOfGuests = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
   const dispatch = useContext(BookingDispatchContext);
 
-
-  const handleClick= (guests: number) => {
+  const handleClick = (guests: number) => {
     // sätt antal gäster via dispatch men förslagsvis ändra om hur bokningen ser ut så att numberOfGuests inte ligger inuti User
     // Här sätts bara antal bord man har bokat
-    
+    dispatch({
+      type: ActionTypeCurrentBooking.SET_NUMBER_OF_GUESTS,
+      payload: guests,
+    });
+
     if (guests < 7) {
-      dispatch({type: ActionTypeCurrentBooking.SET_BOOKED_TABLES, payload: 1});
+      dispatch({
+        type: ActionTypeCurrentBooking.SET_BOOKED_TABLES,
+        payload: 1,
+      });
     }
 
     if (guests > 6) {
-      dispatch({type: ActionTypeCurrentBooking.SET_BOOKED_TABLES, payload: 2});
+      dispatch({
+        type: ActionTypeCurrentBooking.SET_BOOKED_TABLES,
+        payload: 2,
+      });
     }
   };
 
@@ -29,8 +38,10 @@ export const BookGuests = ({ goToCalendar }: IChooseGuests) => {
     <>
       <div>
         <p>Här väljer du antalet gäster</p>
-        {numberOfGuests.map(guests => (
-          <div key={guests} onClick={() => handleClick(guests)}>{guests}</div>
+        {numberOfGuests.map((guests) => (
+          <div key={guests} onClick={() => handleClick(guests)}>
+            {guests}
+          </div>
         ))}
         <button
           type="button"
