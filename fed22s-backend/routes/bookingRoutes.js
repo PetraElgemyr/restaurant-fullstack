1a1bf0a9a089a274bfd893b111520c2934a4d2e8
petraelgemyr <petra.elgemyr@gmail.com>
2023-06-08 16:20:21 +0200

    (feat): add new controller to update booking

--- fed22s-backend/routes/bookingRoutes.js
+++ fed22s-backend/routes/bookingRoutes.js
@@ -2,15 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    getAllBookings,
-    createBooking,
-    deleteBooking,
-    getBookingsByDate
+  getAllBookings,
+  createBooking,
+  deleteBooking,
+  getBookingsByDate,
+  updateBookingById,
 } = require("../controllers/bookingController");
 
 router.get("/", getAllBookings);
 router.get("/:bookingDate", getBookingsByDate);
 router.post("/", createBooking);
 router.delete("/:bookingId", deleteBooking);
+router.put("/:bookingId", updateBookingById);
 
 module.exports = router;
