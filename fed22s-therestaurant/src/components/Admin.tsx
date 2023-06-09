1024a29ca19e48e8c48e8ca8c5b0403997c80a94
Jennifer McAllister <113523615+jennifer-mcallister@users.noreply.github.com>
2023-06-09 12:05:44 +0200

    (feat): add newBooking btn and custom hook

--- fed22s-therestaurant/src/components/Admin.tsx
+++ fed22s-therestaurant/src/components/Admin.tsx
@@ -4,9 +4,12 @@ import "react-calendar/dist/Calendar.css";
 import { BookingComponent } from "./BookingComponent";
 import { getBookingsByDate } from "../serivces/BookingServices";
 import { Booking } from "../models/Booking";
+import { useConvertDateToString } from "../hooks/useConvertDateToString";
+
 
 export const Admin = () => {
   const isAdmin = true;
+  const [createNewBooking, setCreateNewBooking] = useState(false);
   const [selectedDate, setSelectedDate] = useState<string>("");
   const [bookingsAtDate, setBookingsAtDate] = useState<Booking[]>([]);
   const [firstSittingBookings, setFirstSittingBookings] = useState<Booking[]>(
@@ -16,21 +19,9 @@ export const Admin = () => {
     []
   );
 
-  const convertDateToString = (day: Date) => {
-    let month: string = (day.getMonth() + 1).toString();
-    let dateDay: string = day.getDate().toString();
-    if (month.length === 1) {
-      month = "0" + month;
-    }
-    if (dateDay.length === 1) {
-      dateDay = "0" + dateDay;
-    }
-    let chosenDate = day.getFullYear().toString() + month + dateDay;
-    return chosenDate;
-  };
 
   const handleDateClick = async (day: Date) => {
-    let date: string = convertDateToString(day);
+    let date: string = useConvertDateToString(day);
     setSelectedDate(date);
     if (date !== "") {
       console.log("Hämta bokningar för datum:", date);
@@ -55,42 +46,56 @@ export const Admin = () => {
 
   console.log("Bokningar på datumet: ", selectedDate, bookingsAtDate);
 
-  return (
-    <div>
-      <h2>Välj datum för att se bokningar</h2>
-      <Calendar onClickDay={(day) => handleDateClick(day)} />
-      <div>
-        <h6>Bokningar för datumet {selectedDate}</h6>
+  const handleClick = () => {
+    setCreateNewBooking(true);
+  }
+  if (!createNewBooking) {
+    return (
+      <>
         <div>
-          <p>Sittning 1</p>
-          {bookingsAtDate.map((booking, index) => {
-            if (booking.sitting === 1) {
-              return (
-                <div key={index}>
-                  <p>{booking.user.name}</p>
-                  <span>{booking.user.email}</span>
-                  <span>{booking.numberOfGuests} st gäster</span>
-                </div>
-              );
-            }
-          })}
+          <button onClick={handleClick}>Lägg till ny bokning</button>
+          <h2>Välj datum för att se bokningar</h2>
+          <Calendar onClickDay={(day) => handleDateClick(day)} />
+          <div>
+            <h6>Bokningar för datumet {selectedDate}</h6>
+            <div>
+              <p>Sittning 1</p>
+              {bookingsAtDate.map((booking, index) => {
+                if (booking.sitting === 1) {
+                  return (
+                    <div key={index}>
+                      <p>{booking.user.name}</p>
+                      <span>{booking.user.email}</span>
+                      <span>{booking.numberOfGuests} st gäster</span>
+                    </div>
+                  );
+                }
+              })}
+            </div>
+            <div>
+              <p>Sittning 2</p>
+              {bookingsAtDate.map((booking, index) => {
+                if (booking.sitting === 2) {
+                  return (
+                    <div key={index}>
+                      <p>{booking.user.name}</p>
+                      <span>{booking.user.email}</span>
+                      <span>{booking.numberOfGuests} st gäster</span>
+                    </div>
+                  );
+                }
+              })}
+            </div>
+          </div>  
         </div>
-        <div>
-          <p>Sittning 2</p>
-          {bookingsAtDate.map((booking, index) => {
-            if (booking.sitting === 2) {
-              return (
-                <div key={index}>
-                  <p>{booking.user.name}</p>
-                  <span>{booking.user.email}</span>
-                  <span>{booking.numberOfGuests} st gäster</span>
-                </div>
-              );
-            }
-          })}
-        </div>
-      </div>
-      <BookingComponent isAdmin={isAdmin}></BookingComponent>
-    </div>
-  );
+      </>
+    )
+  } else {
+    return (
+      <>
+        <BookingComponent isAdmin={isAdmin}></BookingComponent>
+      </>
+    )
+  }
+
 };
