d1656aee47348716f5bfe84bd259a376adb24d8d
petraelgemyr <petra.elgemyr@gmail.com>
2023-06-12 11:53:29 +0200

    (feat): add settimeout to loading

--- fed22s-therestaurant/src/components/CancelBooking.tsx
+++ fed22s-therestaurant/src/components/CancelBooking.tsx
@@ -1,17 +1,12 @@
 import { Link, useParams } from "react-router-dom";
-import {
-  deleteBookingById,
-  getAllBookings,
-} from "../serivces/BookingServices";
+import { deleteBookingById, getAllBookings } from "../serivces/BookingServices";
 import { useEffect, useState } from "react";
 import { Booking } from "../models/Booking";
-
 
 export const CancelBooking = () => {
   const { bookingId } = useParams<string>();
   const [booking, setBooking] = useState<Booking>();
-
-
+  const [isLoading, setLoading] = useState(true);
 
   const handleDeleteClick = async () => {
     if (bookingId) {
@@ -19,46 +14,51 @@ export const CancelBooking = () => {
     }
   };
 
-
   useEffect(() => {
     const getBooking = async () => {
       const response: Booking[] = await getAllBookings();
       console.log("Respons från api:et: ", response);
       const bookingExists = response.find((booking) => {
         return booking.bookingId === bookingId;
-      })
-      console.log(bookingExists)
-      setBooking(bookingExists)
+      });
+      console.log(bookingExists);
+      setBooking(bookingExists);
+
+      setTimeout(() => {
+        setLoading(false);
+      }, 1500);
     };
 
     getBooking();
   }, []);
 
+  if (isLoading) {
+    return <div>Laddar...</div>;
+  }
 
-    if (booking) {
-      return (
+  return (
+    <>
+      {booking ? (
         <>
-        <h4>Avbokning för bokning {booking.bookingId}</h4>
-        <p>Är du säker på att du vill avboka din bokning?</p>
-  
-        <Link to="/cancel/confirmation">
-          <button type="button" onClick={handleDeleteClick}>
-            Avboka
-          </button>
-        </Link>
-      </>
-      )
-    } else {
-      return (
-        <>
-        <h4>Ojdå! Bokningen kunde inte hittas.</h4>
-        <p>
-          Vi ser inte att er bokning finns i vårt system. Vänligen kontakta oss
-          vid frågor.
-        </p>
-        <span>Vänliga hälsningar, Bakgården</span>
-      </>
-      )
-    }
+          <h4>Avbokning för bokning {booking.bookingId}</h4>
+          <p>Är du säker på att du vill avboka din bokning?</p>
 
+          <Link to="/cancel/confirmation">
+            <button type="button" onClick={handleDeleteClick}>
+              Avboka
+            </button>
+          </Link>
+        </>
+      ) : (
+        <>
+          <h4>Ojdå! Bokningen kunde inte hittas.</h4>
+          <p>
+            Vi ser inte att er bokning finns i vårt system. Vänligen kontakta
+            oss vid frågor.
+          </p>
+          <span>Vänliga hälsningar, Bakgården</span>
+        </>
+      )}
+    </>
+  );
 };
