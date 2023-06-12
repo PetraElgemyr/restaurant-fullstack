6f6e7b8a77ed29ca507a2da0106524d35b8e09b5
Jennifer McAllister <113523615+jennifer-mcallister@users.noreply.github.com>
2023-06-12 11:41:41 +0200

    (fix): add rendering if booking exists or not

--- fed22s-therestaurant/src/components/CancelBooking.tsx
+++ fed22s-therestaurant/src/components/CancelBooking.tsx
@@ -2,20 +2,16 @@ import { Link, useParams } from "react-router-dom";
 import {
   deleteBookingById,
   getAllBookings,
-  getBookingById,
 } from "../serivces/BookingServices";
 import { useEffect, useState } from "react";
-import { Booking, defaultBooking } from "../models/Booking";
-import { AxiosResponse } from "axios";
+import { Booking } from "../models/Booking";
+
 
 export const CancelBooking = () => {
   const { bookingId } = useParams<string>();
-  const [bookingExists, setBookingExists] = useState<boolean>(false);
-  const [bookings, setBookings] = useState<Booking[]>([]);
-  const [bookingToCancel, setBookingToCancel] =
-    useState<Booking>(defaultBooking);
-  const [isLoading, setIsLoading] = useState(true);
-  const [html, setHtml] = useState<JSX.Element>(<></>);
+  const [booking, setBooking] = useState<Booking>();
+
+
 
   const handleDeleteClick = async () => {
     if (bookingId) {
@@ -23,62 +19,46 @@ export const CancelBooking = () => {
     }
   };
 
-  const findBooking = () => {
-    return bookings.find((booking) => {
-      return booking.bookingId === bookingId;
-    });
-  };
-
-  const foundHtml = (
-    <>
-      <h4>Avbokning för bokning {bookingToCancel.bookingId}</h4>
-      <p>Är du säker på att du vill avboka din bokning?</p>
-
-      <Link to="/cancel/confirmation">
-        <button type="button" onClick={handleDeleteClick}>
-          Avboka
-        </button>
-      </Link>
-    </>
-  );
-  const notFoundHtml = (
-    <>
-      <h4>Ojdå! Bokningen kunde inte hittas.</h4>
-      <p>
-        Vi ser inte att er bokning finns i vårt system. Vänligen kontakta oss
-        vid frågor.
-      </p>
-      <span>Vänliga hälsningar, Bakgården</span>
-    </>
-  );
 
   useEffect(() => {
     const getBooking = async () => {
       const response: Booking[] = await getAllBookings();
       console.log("Respons från api:et: ", response);
-      setBookings(response);
-      // setIsLoading(false);
-      // console.log("loading blir false: ", isLoading);
-
-      let bookingData: Booking | undefined = bookings.find((booking) => {
+      const bookingExists = response.find((booking) => {
         return booking.bookingId === bookingId;
-      });
-      if (bookingData) {
-        setBookingToCancel(bookingData);
-        setHtml(<div>Bokningen hittades!</div>);
-        setBookingExists(true);
-      } else {
-        setBookingExists(false);
-        setHtml(<>Bokningen hittades tyvärr inte</>);
-      }
+      })
+      console.log(bookingExists)
+      setBooking(bookingExists)
     };
 
     getBooking();
   }, []);
 
-  // if (isLoading) {
-  //   return <div>Laddar...</div>;
-  // }
 
-  return <>{html}</>;
+    if (booking) {
+      return (
+        <>
+        <h4>Avbokning för bokning {booking.bookingId}</h4>
+        <p>Är du säker på att du vill avboka din bokning?</p>
+  
+        <Link to="/cancel/confirmation">
+          <button type="button" onClick={handleDeleteClick}>
+            Avboka
+          </button>
+        </Link>
+      </>
+      )
+    } else {
+      return (
+        <>
+        <h4>Ojdå! Bokningen kunde inte hittas.</h4>
+        <p>
+          Vi ser inte att er bokning finns i vårt system. Vänligen kontakta oss
+          vid frågor.
+        </p>
+        <span>Vänliga hälsningar, Bakgården</span>
+      </>
+      )
+    }
+
 };
