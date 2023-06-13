2b429059ea41c09f9cc9b38a786c0043cfcecf59
petraelgemyr <petra.elgemyr@gmail.com>
2023-06-14 00:12:27 +0200

    (refactor): remove unused imports

--- fed22s-therestaurant/src/components/CalendarPage.tsx
+++ fed22s-therestaurant/src/components/CalendarPage.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { BookingDispatchContext } from "../contexts/BookingDispatchContext";
 import Calendar from "react-calendar";
 import { ActionTypeCurrentBooking } from "../reducers/CurrentBookingReducer";
--- fed22s-therestaurant/src/components/GdprInfo.tsx
+++ fed22s-therestaurant/src/components/GdprInfo.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
 import { BackButton, Button } from "./styled/Buttons";
 import { WrapperColumn } from "./styled/Wrappers";
 import { GdprSpan } from "./styled/Texts";
