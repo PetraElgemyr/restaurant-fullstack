4605552436f5ca3f61fd937fb99cbcf374ae124f
petraelgemyr <petra.elgemyr@gmail.com>
2023-06-13 14:02:06 +0200

    (feat):  add mediaquery to form & input

--- fed22s-therestaurant/src/components/styled/Forms.tsx
+++ fed22s-therestaurant/src/components/styled/Forms.tsx
@@ -1,39 +1,50 @@
 import styled from "styled-components";
+import { devices } from "./devices";
 
-export const Form = styled.form`
-    display: flex;
-    flex-direction: column;
+export const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+
+  @media screen and (${devices.desktop}) {
     gap: 30px;
-`
+  }
+`;
 
 export const Input = styled.input`
-    font-family: 'Kite One', sans-serif;
-    font-size: 20px;
-    color: white;
-    border: 1px solid rgba(0, 0, 0, 0.30);
-    background-color: #4B5F7B;
-    height: 40px;
-    min-width: 100px;
-    cursor: pointer;
-    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-    transition-property: border background-color;
-    transition-duration: 0.5s;
-    border-radius: 4px;
-    padding: 10px;
-    box-shadow: rgba(0, 0, 0, 0.30) 0px 2px 4px 0px inset;
-      
-    :-webkit-outer-spin-button,
+  font-family: "Kite One", sans-serif;
+  font-size: 20px;
+  color: white;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  background-color: #4b5f7b;
+  height: 30px;
+  min-width: 100px;
+  cursor: pointer;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  transition-property: border background-color;
+  transition-duration: 0.5s;
+  border-radius: 4px;
+  padding: 10px;
+  box-shadow: rgba(0, 0, 0, 0.3) 0px 2px 4px 0px inset;
+
+  :-webkit-outer-spin-button,
     :-webkit-inner-spin-button 
     -webkit-appearance: none;
     margin: 0;
     -moz-appearance: textfield;
 
-    &:focus {
-        border: 1px solid #ACCDFF;
-        outline: white;
-        background-color: #7C95BB;
-        &::placeholder {
-            opacity: 0;
-        }
+  &:focus {
+    border: 1px solid #accdff;
+    outline: white;
+    background-color: #7c95bb;
+    &::placeholder {
+      opacity: 0;
     }
-`
+  }
+
+  @media screen and (${devices.desktop}) {
+    height: 45px;
+  }
+`;
