fc90915b5a5cfdb86ff463c2abd112077c59c8e3
petraelgemyr <petra.elgemyr@gmail.com>
2023-06-13 16:20:16 +0200

    (feat): add a change & delete btn

--- fed22s-therestaurant/src/components/styled/Buttons.tsx
+++ fed22s-therestaurant/src/components/styled/Buttons.tsx
@@ -2,33 +2,48 @@ import { styled } from "styled-components";
 import { devices } from "./devices";
 
 interface IButtonProps {
-    disabled: boolean;
+  disabled: boolean;
 }
 export const Button = styled.button`
-    font-family: 'Julius Sans One', sans-serif;
-    font-size: 20px;
-    color: ${({disabled}) => disabled ? "#C1B9B9" : "white"};
-    border: 1px solid rgba(50, 50, 93, 0.25);
-    background-color: ${({disabled}) => disabled ? "#818C9D" : "#818C9D"};
-    height: 50px;
-    min-width: 100px;
-    cursor: pointer;
-    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-    border-radius: 4px;
-    padding: 20px;
-    display:flex;
-    justify-content: center;
-    align-items: center;
-    transition: all 0.5s;
+  font-family: "Julius Sans One", sans-serif;
+  font-size: 20px;
+  color: ${({ disabled }) => (disabled ? "#C1B9B9" : "white")};
+  border: 1px solid rgba(50, 50, 93, 0.25);
+  background-color: ${({ disabled }) => (disabled ? "#818C9D" : "#818C9D")};
+  height: 50px;
+  min-width: 100px;
+  cursor: pointer;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  border-radius: 4px;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.5s;
 
-    &:hover {
-        border: 1px solid #ACCDFF;    
-    }
+  &:hover {
+    border: 1px solid #accdff;
+  }
 
-    &:active {
-        transition: none;
-        border: ${({disabled}) => disabled ? "1px solid #818C9D" : "1px solid #59739A"}; ;
-        background-color: ${({disabled}) => disabled ? "#818C9D" : "#4B5F7B"};
-        color: ${({disabled}) => disabled ? "#C1B9B9" : "#ACCDFF"};
-    }
-`
+  &:active {
+    transition: none;
+    border: ${({ disabled }) =>
+      disabled ? "1px solid #818C9D" : "1px solid #59739A"};
+    background-color: ${({ disabled }) => (disabled ? "#818C9D" : "#4B5F7B")};
+    color: ${({ disabled }) => (disabled ? "#C1B9B9" : "#ACCDFF")};
+  }
+`;
+
+export const ChangeButton = styled(Button)`
+  font-size: 11pt;
+  padding: 5px;
+  height: 40px;
+  min-width: 50px;
+  margin: 5px;
+
+  @media screen and (${devices.tablet}) {
+    width: 140px;
+    font-size: 13pt;
+    padding: 25px;
+  }
+`;
