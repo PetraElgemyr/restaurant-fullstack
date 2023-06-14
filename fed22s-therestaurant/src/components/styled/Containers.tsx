6c921bd9511cd933bcba279427361e38ae13abc2
Jennifer McAllister <113523615+jennifer-mcallister@users.noreply.github.com>
2023-06-14 14:36:43 +0200

    (feat): add styling welcomepage

--- fed22s-therestaurant/src/components/styled/Containers.tsx
+++ fed22s-therestaurant/src/components/styled/Containers.tsx
@@ -6,15 +6,22 @@ interface IContainerProps {
     flexdirection?: string;
 } 
 
+interface IImageContainerProps {
+    img?: string;
+}
+
 export const Container = styled.div<IContainerProps>`
     display: flex;
     flex-direction: ${(props: IContainerProps) => props.flexdirection || "column"};
     justify-content: center;
     align-items: center;
     gap: 20px;
+    border-radius: 6px;
+    min-width: 50vw;
+    max-width: 80vw;
 `
 
-export const ImageContainer = styled.div `
+export const ImageContainer = styled.div<IImageContainerProps> `
     display: none;
     justify-content: center;
     align-items: center;
@@ -23,7 +30,7 @@ export const ImageContainer = styled.div `
 
     @media screen and (${devices.desktop}) {
         display: flex;
-        background-image: url("src/assets/plate-figgs.jpg");
+        background-image: url(${(props: IImageContainerProps) => props.img || ""});
         background-position: center;
         background-size: cover;
     }
