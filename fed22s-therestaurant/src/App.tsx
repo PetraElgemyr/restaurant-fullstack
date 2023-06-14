708dbb4aeb5e493e98be85df4a936a5e3c486503
Jennifer McAllister <113523615+jennifer-mcallister@users.noreply.github.com>
2023-06-14 20:20:01 +0200

    (feat): add loading page at start and data loading

--- fed22s-therestaurant/src/App.tsx
+++ fed22s-therestaurant/src/App.tsx
@@ -1,15 +1,30 @@
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router";
+import { Loader } from "./components/Loader";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
-  useEffect(() => {}, []);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    },2500)
+  }, []);
 
-  return (
-    <>
-      <RouterProvider router={router}></RouterProvider>
-    </>
-  );
+  if (loading) {
+    return (
+      <>
+        <Loader/>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <RouterProvider router={router}></RouterProvider>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ fed22s-therestaurant/src/components/Loader.tsx
@@ -0,0 +1,26 @@
+import { H2 } from "./styled/Headings"
+import { LoaderContainer, Spinner } from "./styled/Loadings"
+import { Span } from "./styled/Span"
+
+export const Loader = () => {
+    return (
+        <LoaderContainer>
+            <Spinner>
+                <svg xmlns="http://www.w3.org/2000/svg"  id="rotatingText" viewBox="0 0 200 200" width="400" height="400">
+                    <defs>
+                        <path id="circle" d="M 100, 100
+                            m -75, 0
+                            a 75, 75 0 1, 0 150, 0
+                            a 75, 75 0 1, 0 -150, 0
+                        ">
+                        </path>
+                    </defs>
+                    <text width="800" className="curved-text">           
+                        <textPath alignment-baseline="left" href="#circle">BakgårdenBakgården</textPath>
+                    </text>
+                </svg>  
+            </Spinner>
+        </LoaderContainer>
+
+    )
+}
